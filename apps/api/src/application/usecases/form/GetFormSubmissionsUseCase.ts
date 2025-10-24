f6a8928f29cab0fad290ef96b4862e0bf445df41
Deivid <deivid.cardos@gmail.com>
2025-10-23 21:26:30 -0300

    feat(api): Serve form submission files via signed URLs

--- apps/api/src/application/usecases/form/GetFormSubmissionsUseCase.ts
+++ apps/api/src/application/usecases/form/GetFormSubmissionsUseCase.ts
@@ -2,14 +2,17 @@ import { NotAllowedError } from '@application/errors/application/NotAllowedError
 import { ResourceNotFound } from '@application/errors/application/ResourceNotFound';
 import { GetFormSubmissionsQuery } from '@application/queries/GetFormSubmissionsQuery';
 import { FormRepository } from '@infra/database/dynamo/repositories/FormRepository';
+import { StorageGateway } from '@infra/gateways/StorageGateway';
 import { Injectable } from '@kernel/decorators/Injectable';
+import { QuestionType } from '@monorepo/shared/enums/QuestionType';
 
 @Injectable()
 export class GetFormSubmissionsUseCase {
   constructor(
     private readonly formRepository: FormRepository,
     private readonly getFormSubmissionsQuery: GetFormSubmissionsQuery,
-  ) {}
+    private readonly storageGateway: StorageGateway,
+  ) { }
 
   async execute({
     formId,
@@ -25,7 +28,31 @@ export class GetFormSubmissionsUseCase {
       throw new NotAllowedError();
     }
 
-    return this.getFormSubmissionsQuery.execute(formId);
+    const { questions, submissions } = await this.getFormSubmissionsQuery.execute(
+      formId,
+    );
+
+    const fileQuestions = questions.filter(
+      (q) => q.questionType === QuestionType.FILE,
+    );
+
+    if (fileQuestions.length > 0) {
+      await Promise.all(
+        submissions.flatMap(({ answers }) =>
+          answers
+            .filter((answer) =>
+              fileQuestions.some((q) => q.id === answer.questionId),
+            )
+            .map(async (answer) => {
+              answer.value = await this.storageGateway.getSignedUrl(
+                answer.value as string,
+              );
+            }),
+        ),
+      );
+    }
+
+    return { questions, submissions };
   }
 }
 
